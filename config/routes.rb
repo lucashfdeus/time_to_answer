baf589ece61ac65cfe51575aef9a59596857881e
Lucas Henrique <lucas.hfdeus@gmail.com>
2024-02-23 22:02:52 +0000

    crindo projeto configurando layout, melhorando rotas e ativando i18n.

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
+
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+
+  devise_for :users
+  devise_for :admins
+
+  get 'inicio', to: 'site/welcome#index'
+
+  root to: 'site/welcome#index'
+end
